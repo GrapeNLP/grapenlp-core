a3bda541704aa6293c1c3967593b986beca33ff7
Sastre Martinez, J <j.sastre.martinez@accenture.com>
2021-02-10 09:07:38 +0000

    build.sh script generates packages for Debian distributions too

--- delivery/scripts/build.sh
+++ delivery/scripts/build.sh
@@ -39,7 +39,7 @@ make test
 success_or_exit
 
 # On Ubuntu, make the Debian packages
-if [[ "$DISTRO_ID" == "ubuntu" ]]; then
+if [ "$DISTRO_ID" == "debian" ] || ["$DISTRO_ID" == "ubuntu" ]; then
   log_info_banner "Packaging"
   make package
   success_or_exit
@@ -48,8 +48,8 @@ fi
 # Report success
 log_info_banner "Build completed"
 
-# On Ubuntu, inform about the location of the resulting Debian packages
-if [[ "$DISTRO_ID" == "ubuntu" ]]; then
+# On Debian or Ubuntu, inform about the location of the resulting Debian packages
+if [ "$DISTRO_ID" == "debian" ] || ["$DISTRO_ID" == "ubuntu" ]; then
   echo "You may now install the packages at ${BUILD_FOLDER}/package:"
   echo
   echo "sudo dpkg -i \"${BUILD_FOLDER}/package/*.deb\""
